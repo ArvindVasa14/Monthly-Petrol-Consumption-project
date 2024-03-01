cabbb8ce40341827edf873c7fdd07a3cba54c215
arvindvasa1122@gmail.com <arvindaiml14@gmail.com>
2024-03-01 09:45:16 +0530

    third commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -23,37 +23,37 @@ class Data_ingestion:
         # Reading properties from yaml file
         try:
             logging.info("Data Ingestion Started")
-            # format1= self.utils.get_property("format1")
-            # offset1= self.utils.get_property("offset1")
-            # limit1= self.utils.get_property("limit1")
+            format1= self.utils.get_property("format1")
+            offset1= self.utils.get_property("offset1")
+            limit1= self.utils.get_property("limit1")
 
-            # response= self.get_data(format1, offset1, limit1)
-            # # Check if the request was successful (status code 200)
-            # if response.status_code == 200:
-            #     logging.info("JSON format data collection started")
-            #     data = response.json() 
-            #     with open('artifacts/json_response.json', 'w') as response_file:
-            #         json.dump(data, response_file, indent=2)
-            #     print("got response json")
-            # else:
-            #     # Handle errors
-            #     print(f"Error: {response.status_code} - {response.text}")
+            response= self.get_data(format1, offset1, limit1)
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                logging.info("JSON format data collection started")
+                data = response.json() 
+                with open('artifacts/json_response.json', 'w') as response_file:
+                    json.dump(data, response_file, indent=2)
+                print("got response json")
+            else:
+                # Handle errors
+                print(f"Error: {response.status_code} - {response.text}")
 
-            # format2= self.utils.get_property("format2")
-            # offset2= self.utils.get_property("offset2")
-            # limit2= self.utils.get_property("limit2")
+            format2= self.utils.get_property("format2")
+            offset2= self.utils.get_property("offset2")
+            limit2= self.utils.get_property("limit2")
 
-            # response= self.get_data(format2, offset2, limit2)
-            # # Check if the request was successful (status code 200)
-            # if response.status_code == 200:
-            #     logging.info("CSV format data collection started")
-            #     data = response.text 
-            #     with open('artifacts/csv_response.csv', 'w') as response_file:
-            #         response_file.write(data)
-            #     print("got response csv")
-            # else:
-            #     # Handle errors
-            #     print(f"Error: {response.status_code} - {response.text}")
+            response= self.get_data(format2, offset2, limit2)
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                logging.info("CSV format data collection started")
+                data = response.text 
+                with open('artifacts/csv_response.csv', 'w') as response_file:
+                    response_file.write(data)
+                print("got response csv")
+            else:
+                # Handle errors
+                print(f"Error: {response.status_code} - {response.text}")
                 
             format3= self.utils.get_property("format3")
             offset3= self.utils.get_property("offset3")
